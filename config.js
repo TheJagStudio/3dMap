13b11893de141985ecb98cbbaf62ba8a1c1ffe52
TheJagStudio <81472856+TheJagStudio@users.noreply.github.com>
2023-02-19 20:12:58 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+var config = {
+	accessToken: "pk.eyJ1IjoiZGV2cGF0ZWwzMjciLCJhIjoiY2xkdmxpcW5tMGgzOTNwcG5qMWFvbzZrMCJ9.P3EXduadE-bAqd0nkBNGkQ",
+	subscriptionKey: "azure maps subscription key goes here",
+};
